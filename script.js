038d103f8af366c8cf78c9c7b4c42eb91e8e6dc8
William Copeland <willcopeland34@gmail.com>
2017-10-15 20:47:33 -0700

    Housekeeping

--- script.js
+++ script.js
@@ -14,43 +14,35 @@ var keyWords = ["landscape","technology","outdoor", "cool", "sky", "water","dog"
 
 
 function nQ() { //New Quote function. Also pulls new picture
-
 	var num = Math.floor(Math.random()* keyWords.length); //picks random keyword
-		
-	$('.view').css("background" , url + keyWords[num] +'")'); //sets CSS image to new URL
-			
-		$.get("https://talaikis.com/api/quotes/random", function(data) { //request new quote
-			
+	
+	$('.view').css("background" , url + keyWords[num] +'")'); //sets CSS image to new URL		
+		$.get("https://talaikis.com/api/quotes/random", function(data) { //request new quote		
 			$('.quote').remove(); //remove old quote while retrieving new
 			$('.author').remove();
 			
 			$('.content').prepend("<div class='quote animated fadeIn lead'>" + data.quote + "</div>"); //puts quotes&author in html
 			$('.content').prepend("<div class='author animated fadeIn h1-responsive'>~" + data.author + "</div>");
-		});
-		
+		});	
 };
-
 
 
 function postTweet(quote, author) {
 	window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" ' + author));
-
 	console.log('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '"' + author));
 };
 
 
 $(document).ready(function() {
 	nQ(); //Loads first quote/image
-
+	
 	//Pulls new quote when "New Quote" button is clicked
 	$('#newQuote').on('click', nQ);
 		
 	//Combines "author" and Wiki base URL into a complete URL
 	$('#search').on('click', function() {
 		author = $('.author').html().substr(1).replace(/ /g,"_");
-		
 		newUrl = wikiURL + author;
-		
 		window.open(newUrl); 
 	});
 	
@@ -60,5 +52,4 @@ $(document).ready(function() {
 		author = $('.author').html();
 		postTweet(quote, author);
 	});
-	
 });
